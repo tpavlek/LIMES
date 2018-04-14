85842ff40dc8cd75226c0c32872d213088514534
Cortland D <cortland@ualberta.ca>
2018-04-14 12:24:02 -0600

    added maps, resolved conflicts (#14)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Location;
 
 class HomeController
 {
 
     public function index()
     {
-        return view('index');
+        $locations = Location::all()
+            ->map(function($location){ return [$location->name, $location->lat, $location->lon];});
+
+        return view('index')->with("locations", json_encode($locations));
     }
 
 }
