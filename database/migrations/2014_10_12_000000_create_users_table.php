3c4421fd85c5c743db40d504acb77cbc751f282d
Troy Pavlek <troy@tpavlek.me>
2018-04-15 14:59:00 -0600

    updated users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 140)->unique();
             $table->string('snapchat')->nullable()->default(null);
             $table->string('twitter')->nullable()->default(null);
             $table->string('password');
